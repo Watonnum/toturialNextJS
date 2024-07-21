e56be5fe0b2e4af791c683929eb6d007824386ac
Watanyu onnum <ipae@MacBook-Air-khxng-Watanyu.local>
2024-07-21 17:40:23 +0700

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
-'use client';
+// 'use client';
 
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-interface FooProps {
-  count: number;
-}
+// interface FooProps {
+//   count: number;
+// }
 
-const Foo = ({ count }: FooProps) => {
-  useEffect(() => {
-    console.log(123);
-  });
-  return <div>{count}</div>;
-};
+// const Foo = ({ count }: FooProps) => {
+//   useEffect(() => {
+//     console.log(123);
+//   });
+//   return <div>{count}</div>;
+// };
 
-const HomePage = () => {
-  const [count, setCount] = useState(0);
+// const HomePage = () => {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <button onClick={() => setCount(count + 1)}>Click to Inc</button>
-      <Foo count={count}></Foo>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <button onClick={() => setCount(count + 1)}>Click to Inc</button>
+//       <Foo count={count}></Foo>
+//     </>
+//   );
+// };
 
-export default HomePage;
+// export default HomePage;
