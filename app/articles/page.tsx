4d275fa730521ed3a5c120db9345db0d9f9a4c43
Watanyu onnum <ipae@MacBook-Air-khxng-Watanyu.local>
2024-07-21 22:52:56 +0700

    complete_17_Server-rendering

---
+++ app/articles/page.tsx
@@ -0,0 +1,11 @@
+import { findAll } from '@/features/articles/api';
+import ArticleList from '@/features/articles/components/ArticleList';
+
+const ArticlesPage = async () => {
+  const articles = await findAll();
+  return <ArticleList articles={articles}></ArticleList>;
+};
+
+export default ArticlesPage;
+
+export const revalidate = 15;
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,5 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-
-interface Article {
-  id: number;
-  title: string;
-}
-
-function useFetch<T>(url: string) {
-  const [data, setData] = useState<T[]>([]);
-
-  const fetchArticles = async () => {
-    const res = await fetch(url);
-    const data = await (res.json() as Promise<T[]>);
-
-    setData(data);
-  };
-
-  useEffect(() => {
-    fetchArticles();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return data;
-}
-
-const ArticlePage = () => {
-  const articles = useFetch<Article>('http://localhost:5151/articles');
-  return (
-    <ul>
-      {articles.map((article) => (
-        <li key={article.id}>{article.title}</li>
-      ))}
-    </ul>
-  );
+const HomePage = () => {
+  return <div>Home</div>;
 };
 
-export default ArticlePage;
+export default HomePage;
